d2bb7621847b5b843db81e89774a073f43ea29fa
helderrezende <helderfgv@gmail.com>
2019-10-22 14:07:19 -0300

    fix: removed old function

--- utils.py
+++ utils.py
@@ -47,22 +47,3 @@ def transform_float_to_datetime(data, columns, format_dt, dayfirst):
         data[col] = pd.to_datetime(data[col], dayfirst=dayfirst, format=format_dt, errors='coerce')
    
     return data
-
-def get_municipio_info(data, columns_cod):
-    municipio = pd.read_csv('../data/municipios.csv', sep=',')
-
-    municipio['codigo_ibge'] = municipio['codigo_ibge'].astype(str)
-    municipio['codigo_ibge'] = municipio['codigo_ibge'].str[:-1] # numero verificador removed
-    municipio['codigo_ibge'] = pd.to_numeric(municipio['codigo_ibge'])
-
-
-    for col in columns_cod:
-        municipio_col = municipio.copy()
-        municipio_col.columns = ['{0}_{1}'.format(col, x) for x in municipio_col]
-        municipio_col[col] = municipio_col['{0}_{1}'.format(col, 'codigo_ibge')]
-
-        data = data.merge(municipio_col, how='left', on=col) 
-
-        data = data.drop('{0}_{1}'.format(col, 'codigo_ibge'), 1)
-    
-    return data
