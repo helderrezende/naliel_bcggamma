ec30ad79a758abb679f8aeec51bdb9f1c11f9070
helderrezende <helderfgv@gmail.com>
2019-10-22 15:12:27 -0300

    feat: cep merge function

--- external_data.py
+++ external_data.py
@@ -22,14 +22,24 @@ def get_municipio_info(data, columns_cod):
     
     return data
 
-def get_cep_info():
+def get_cep_info(data, columns_cep):
     """Source: http://cep.la/baixar
     
     """
-    data = pd.read_csv('../data/ceps-latin1.txt', encoding='latin1',
-                       sep='\t', names=['CEP', 'Municipio', 'Bairro', 'Complemento'])
+    cep_df = pd.read_csv('../data/ceps-latin1.txt', encoding='latin1',
+                       sep='\t', names=['CEP', 'MUNICIPIO', 'BAIRRO', 'COMPLEMENTO'])
 
-    data['CEP'] = data['CEP'].astype(str).str.zfill(8)
+    cep_df['CEP'] = cep_df['CEP'].astype(str).str.zfill(8)
+    cep_df = cep_df.drop('MUNICIPIO', 1)
+    
+    for col in columns_cep:
+        cep_col = cep_df.copy()
+        cep_col.columns = ['{0}_{1}'.format(col, x) for x in cep_col]
+        cep_col[col] = cep_col['{0}_{1}'.format(col, 'CEP')]
+
+        data = data.merge(cep_col, how='left', on=col) 
+
+        data = data.drop('{0}_{1}'.format(col, 'CEP'), 1)
     
     
     return data
